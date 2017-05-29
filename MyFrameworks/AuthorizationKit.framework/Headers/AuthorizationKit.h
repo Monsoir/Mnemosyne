3b2b2cdb81ad8f8b79a07548ed2170d310330db2
Yeung Mon <monwingyeung@gmail.com>
2017-05-29 14:56:05 +0800

    添加 Framework FileManagerShortcutKit AuthorizationKit

---
+++ MyFrameworks/AuthorizationKit.framework/Headers/AuthorizationKit.h
@@ -0,0 +1,19 @@
+//
+//  AuthorizationKit.h
+//  AuthorizationKit
+//
+//  Created by Mon on 29/05/2017.
+//  Copyright © 2017 wenyongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuthorizationKit.
+FOUNDATION_EXPORT double AuthorizationKitVersionNumber;
+
+//! Project version string for AuthorizationKit.
+FOUNDATION_EXPORT const unsigned char AuthorizationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthorizationKit/PublicHeader.h>
+
+
---
+++ MyFrameworks/FileManagerShortcutKit.framework/Headers/FileManagerShortcutKit.h
@@ -0,0 +1,19 @@
+//
+//  FileManagerShortcutKit.h
+//  FileManagerShortcutKit
+//
+//  Created by Mon on 29/05/2017.
+//  Copyright © 2017 wenyongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileManagerShortcutKit.
+FOUNDATION_EXPORT double FileManagerShortcutKitVersionNumber;
+
+//! Project version string for FileManagerShortcutKit.
+FOUNDATION_EXPORT const unsigned char FileManagerShortcutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileManagerShortcutKit/PublicHeader.h>
+
+
